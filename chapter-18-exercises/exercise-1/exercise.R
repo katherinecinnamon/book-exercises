1297d253854b71169166bea741b1e01c38e66fe3
Katherine Cinnamon <katcinna@uw.edu>
2020-02-14 14:12:40 -0800

    Adding week 6 exercises

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -1,3 +1,6 @@
+#Katherine Cinnamon
+#Chapter 18, exercise 1
+
 # load relevant libraries
 library("httr")
 library("jsonlite")
@@ -7,9 +10,12 @@ library("jsonlite")
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
+source("apikey.R") #selects all in that file and ctrl-enters
 
+query_params <- list("api_key" = nyt_key)
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
+movie_name <- "Parasite"
 
 
 # Construct an HTTP request to search for reviews for the given movie.
@@ -20,26 +26,40 @@ library("jsonlite")
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+endpoint <- "reviews/search.json"
+uri <- paste0(base_uri, endpoint)
 
-
+query_params_list <- list("api-key" = nyt_key)
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
-
+response <- GET(uri, query = query_params_list)
+body <- content(response, "text")
+data <- fromJSON(body)
+data_df <- data.frame(data)
 # What kind of data structure did this produce? A data frame? A list?
 
+#data frame
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+View(data_df)
+print(colnames(data_df))
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten(data_df)
+View(reviews)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
-
+recent <- reviews[1, ]
+headline <- recent$results.headline
+short_summary <- recent$results.summary_short
+link <- recent$results.link.url
+View(recent)
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+info_list <- list(headline, short_summary, link)
+print(info_list)
+
